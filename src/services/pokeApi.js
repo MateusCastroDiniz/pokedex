6f165d3e419cebb769810b8fd56c349a66b8d135
Mateus Diniz <serv.mateus@gmail.com>
2026-01-02 17:31:17 -0300

    feat: adicionando services

---
+++ src/services/pokeApi.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'https://pokeapi.co/api/v2/',
+})
+
+export async function getPokemons(limit = 51){
+    const response = await api.get(`/pokemon?limit=${limit}`)
+    return response.data.results;
+}
+
+export async function getPokemonDetails(url){
+    const response = await axios.get(url);
+    return response.data;
+}
+
+export async function getPokemonByNameOrId(term){
+    const response = await api.get(`/pokemon/${term}`);
+    return response.data;
+}
