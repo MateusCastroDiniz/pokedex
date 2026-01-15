275fd9ebcc385d52b40f351233921680ca405c65
Mateus Diniz <serv.mateus@gmail.com>
2026-01-15 17:09:22 -0300

    feat: implementação de camada de estilo de fonte para o MUI

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import './styles/index.css';
+// import './styles/index.css';
 import App from './App.jsx';
-
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "./utils/theme.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
