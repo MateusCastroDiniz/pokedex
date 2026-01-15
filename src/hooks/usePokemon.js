7515ee5490635eaa2a11a7a07be8db68b6052d50
Mateus Diniz <serv.mateus@gmail.com>
2026-01-15 12:40:54 -0300

    feat: adicionando efetividade de tipos em um pokemon

--- src/hooks/usePokemon.js
+++ src/hooks/usePokemon.js
@@ -3,6 +3,7 @@ import {getPokemonByNameOrId, getSpecieDetail, getTypeDetail} from "../services/
 import { getColorBySpecieId } from "../utils/speciesColor";
 import {statRangeCalc} from "../utils/statRangeCalc"
 import { calculateTypeEffectiveness } from "../utils/calcTypeEffectiveness";
+import { getTypeColor } from "../utils/colorScheme";
 
 export function usePokemon(nameOrId){
     const [pokemon, setPokemon] = useState(null);
@@ -29,10 +30,16 @@ export function usePokemon(nameOrId){
                 hp: 'HP',
                 attack: 'Attack',
                 defense: 'Defense',
-                speed: 'Speed'
+                speed: 'Speed',
+                'special-attack': 'Special Attack',
+                'special-defense': 'Special Defense',
                 };
 
-                const statWrapMap = {
+                const statKeyMap = {
+                'hp': 'HP',
+                'attack': 'Attack',
+                'defense': 'Defense',
+                'speed': 'Speed',
                 'special-attack': 'Sp. Atk.',
                 'special-defense': 'Sp. Def.',
                 };
@@ -40,16 +47,6 @@ export function usePokemon(nameOrId){
                 const formatGrowthRate = (rate) =>
                 rate.replace('-', ' ').replace(/\b\w/g, l => l.toUpperCase());
 
-                data.stats = data.stats.map(s => {
-                    const isHP = s.stat.name === "hp";
-
-                    s.stat.name = statLabelMap[s.stat.name] ?? statWrapMap[s.stat.name]
-
-                    return{
-                        ...s,
-                        range: statRangeCalc(s.base_stat, isHP)
-                    }
-                })
                 
                 data.meta = {
                     baseExp: data.base_experience,
@@ -57,14 +54,26 @@ export function usePokemon(nameOrId){
                     .filter(s => s.effort > 0)
                     .map(s => ({
                         value: s.effort,
-                        key: s.stat.name,
+                        key: statKeyMap[s.stat.name] ?? s.stat.name,
                         label: statLabelMap[s.stat.name] ?? s.stat.name
                     })),
                     catchRate: specieDetail.capture_rate,
                     baseFriendship: specieDetail.base_happiness,
                     growthRate: formatGrowthRate(specieDetail.growth_rate.name)
                 };
+                
+                data.stats = data.stats.map(s => {
+                    const isHP = s.stat.name === "hp";
+                    const statName = s.stat.name
 
+                    s.stat.name = statLabelMap[statName] ?? statName
+                    s.stat.nameWrap =  statKeyMap[statName] ?? statName
+
+                    return{
+                        ...s,
+                        range: statRangeCalc(s.base_stat, isHP)
+                    }
+                })
                 
                 data.typesDetails = await Promise.all(
                 data.types.map(t => getTypeDetail(t.type.name))
