221b053a31965838b063d1c7361acd97ada8cd03
Mateus Diniz <serv.mateus@gmail.com>
2026-01-15 12:42:21 -0300

    feat: adicionando biblioteca com tipos de pokemons

---
+++ src/utils/types.js
@@ -0,0 +1,6 @@
+export const ALL_TYPES = [
+  'normal', 'fire', 'water', 'grass', 'electric', 'ice',
+  'fighting', 'poison', 'ground', 'flying',
+  'psychic', 'bug', 'rock', 'ghost',
+  'dragon', 'dark', 'steel', 'fairy'
+];
