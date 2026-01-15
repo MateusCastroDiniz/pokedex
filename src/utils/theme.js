58b53b5056d7f056afaf6526ac4c96a5a2754e1e
Mateus Diniz <serv.mateus@gmail.com>
2026-01-15 17:10:28 -0300

    feat: criação de camada de tema para fontes

---
+++ src/utils/theme.js
@@ -0,0 +1,10 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+    typography:{
+        fontWeightRegular: 400,
+        fontWeightMedium: 500,
+        fontWeightBold: 700,
+        fontFamily: `'sf-pro', system-ui, -apple-system, BlinkMacSystemFont, sans-serif`,
+    }
+})
